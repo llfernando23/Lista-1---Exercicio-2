1d7b2bb329e37aef73d4f460db5677808badf9db
llfernando23 <115510601+llfernando23@users.noreply.github.com>
2022-10-10 20:35:14 -0300

    Add files via upload

---
+++ Exercicio 2/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void  main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int numero = sc.nextInt();
+
+        System.out.println("O número digitado foi:");
+        System.out.println(numero);
+    }
+}
